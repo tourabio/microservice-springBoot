baf3b626bf2c9fbfde84f683795a95691b777ef0
Iheb Rejeb <iheb.rejeb@esprit.tn>
2021-10-26 23:02:39 +0100

    update ms node

--- nodejs-ms/server.js
+++ nodejs-ms/server.js
@@ -1,28 +1,26 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const eurekaConfig = require('./eurekaConfig')
-var cors = require('cors')
-const dotenv = require('dotenv')
+const express = require("express");
+const eurekaConfig = require("./eurekaConfig");
+var cors = require("cors");
 
-dotenv.config()
-const con = require('./database/db')
-const userApi = require('./routes/userApi')
+require("./database/db");
+eurekaConfig.registerWithEureka();
+const userApi = require("./routes/userApi");
 
-const app = express()
-//eurekaConfig.registerWithEureka()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/api/v1', userApi)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", userApi);
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+);
